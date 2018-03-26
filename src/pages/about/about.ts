0073524c6a598f5d7dc840fb1cf4cbd10141a41d
inanfatih <inanfatih@users.noreply.github.com>
2018-03-25 20:35:50 -0400

    Modal is created but intent is not ready yet

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor( public modalCtrl: ModalController) { }
+
+  openModal(characterNum){
+
+    let modal = this.modalCtrl.create('ModalPage', characterNum);
+    modal.present();
 
   }
 
+
+  
 }
+
+
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
+
 export class ContactPage {
-
+  items;
   constructor(public navCtrl: NavController) {
-
+    this.initializeItems();
   }
 
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+      'Buenos Aires',
+      'Cairo',
+      'Dhaka',
+      'Edinburgh',
+      'Geneva',
+      'Genoa',
+      'Glasglow',
+      'Hanoi',
+      'Hong Kong',
+      'Islamabad',
+      'Istanbul',
+      'Washington'
+    ];
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
+
+
+
+
+
