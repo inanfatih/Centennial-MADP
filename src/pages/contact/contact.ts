46662cdc0b4f667083abf561f68cc88ea70d957d
inanfatih <inanfatih@users.noreply.github.com>
2018-03-26 23:47:29 -0400

    instructors are listed in cards. Filtering functionality is not working.

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -12,15 +12,52 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {
     this.initializeItems();
   }
+  instructors;
 
   initializeItems() {
-    this.items = [
-      'Irvin Kaur',
-      'Kamalpreet Singh',
-      'Robert Argume',
-      'Fatih Inan'
+
+    
+    this.instructors = [
+      {
+        instructor: 'Instructor1',
+        courses: 'MADP-711',
+        email: 'email@centennialcollege.ca',
+        phone: '416 123 4567'
+      },
+      {
+        instructor: 'Instructor2',
+        courses: 'MADP-711',
+        email: 'email@centennialcollege.ca',
+        phone: '416 123 4567'
+      },
+      {
+        instructor: 'Instructor3',
+        courses: 'MADP-711',
+        email: 'email@centennialcollege.ca',
+        phone: '416 123 4567'
+      },
+      {
+        instructor: 'Instructor4',
+        courses: 'MADP-711',
+        email: 'email@centennialcollege.ca',
+        phone: '416 123 4567'
+      },
+      {
+        instructor: 'Instructor5',
+        courses: 'MADP-711',
+        email: 'email@centennialcollege.ca',
+        phone: '416 123 4567'
+      },
+      {
+        instructor: 'Instructor6',
+        courses: 'MADP-711',
+        email: 'email@centennialcollege.ca',
+        phone: '416 123 4567'
+      }
     ];
   }
+
+    
 
   getItems(ev) {
     // Reset items back to all of the items
@@ -31,12 +68,90 @@ export class ContactPage {
 
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
+      this.instructors = this.instructors.filter((item) => {
         return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     }
   }
 }
+
+
+
+//html de asagidaki gibi geciyor filtreleme kismi
+
+// <div class='col-md-4'>
+// <input type='text' [(ngModel)]='listFilter' />
+// </div>
+
+
+///This will be used for filtering:
+
+// import { Component, OnInit } from '@angular/core';
+// import { IProduct } from './product';
+
+// @Component({
+//     selector: 'pm-products',
+//     templateUrl: './product-list.component.html',
+//     styleUrls: ['./product-list.component.css']
+// })
+// export class ProductListComponent implements OnInit {
+//     pageTitle: string = 'Product List';
+//     imageWidth: number = 50;
+//     imageMargin: number = 2;
+//     showImage: boolean = false;
+
+//     _listFilter: string;
+//     get listFilter(): string {
+//         return this._listFilter;
+//     }
+//     set listFilter(value: string) {
+//         this._listFilter = value;
+//         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+//     }
+
+//     filteredProducts: IProduct[];
+//     products: IProduct[] = [
+//         {
+//             "productId": 2,
+//             "productName": "Garden Cart",
+//             "productCode": "GDN-0023",
+//             "releaseDate": "March 18, 2016",
+//             "description": "15 gallon capacity rolling garden cart",
+//             "price": 32.99,
+//             "starRating": 4.2,
+//             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+//         },
+//         {
+//             "productId": 5,
+//             "productName": "Hammer",
+//             "productCode": "TBX-0048",
+//             "releaseDate": "May 21, 2016",
+//             "description": "Curved claw steel hammer",
+//             "price": 8.9,
+//             "starRating": 4.8,
+//             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+//         }
+//     ];
+
+//     constructor() {
+//         this.filteredProducts = this.products;
+//         this.listFilter = 'cart';
+//     }
+
+//     performFilter(filterBy: string): IProduct[] {
+//         filterBy = filterBy.toLocaleLowerCase();
+//         return this.products.filter((product: IProduct) =>
+//               product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+//     }
+
+//     toggleImage(): void {
+//         this.showImage = !this.showImage;
+//     }
+
+//     ngOnInit(): void {
+//         console.log('In OnInit');
+//     }
+// }
 
 
 
