f6c9d8d7dd1bd8028f94e58451f4b201c87365d4
singh.kamall <preetkamal110@gmail.com>
2018-04-01 14:58:18 -0400

    Comments page implemented
    Users can view comments.
    Users can add comments.

--- src/pages/comments/comments.ts
+++ src/pages/comments/comments.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, DateTime } from 'ionic-angular';
+import * as moment from 'moment';
 
 /**
  * Generated class for the CommentsPage page.
@@ -14,14 +15,68 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'comments.html',
 })
 export class CommentsPage {
+  
+  items;
+  comments:String;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CommentsPage');
   }
 
+  initializeItems() {
+
+    this.items = [
+      {
+        commentBy: 'Robert',
+        comments: 'MAPD-712, MADP-722',
+        createdOn: 'April 01, 2018 01:33 PM'
+      },
+      {
+        commentBy: 'Fatih',
+        comments: 'MAPD-712, MADP-722',
+        createdOn: 'April 01, 2018 01:14 PM'
+      },
+      {
+        commentBy: 'Kamalpreet Singh',
+        comments: 'MAPD-712, MADP-722',
+        createdOn: 'April 01, 2018 01:06 PM'
+      },
+      {
+        commentBy: 'Kamalpreet Singh',
+        comments: 'MAPD-712, MADP-722',
+        createdOn: 'April 01, 2018 01:00 PM'
+      }
+    ];
+  }
+
+  saveComments() {
+    if (this.comments.trim().length > 0){
+
+      let comment = {
+        commentBy: 'Kamalpreet Singh',
+        comments: this.comments.trim(),
+        createdOn: moment().format('MMM DD, YYYY hh:mm A')
+      };
+
+      let tempItems = this.items;
+      this.items = [];
+      this.items.push(comment);
+
+      tempItems.forEach(element => {
+        this.items.push({
+          "commentBy": element.commentBy,
+          "comments": element.comments,
+          "createdOn": element.createdOn
+        });
+      });
+
+      tempItems = null;
+
+      this.comments = "";
+    }
+  }
 }
-
-
